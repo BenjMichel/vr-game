67c62ee420a7f585ad96458762c293aa0d9eae55
Benjamin Michel <benjamin.michel24@gmail.com>
2016-03-18 00:12:48 +0100

    Bind gyro with threejs

---
+++ src/1_gyro.js
@@ -0,0 +1,20 @@
+var initDeviceOrientationWatcher = function(callback) {
+  if (window.DeviceOrientationEvent) {
+    // Listen for the deviceorientation event and handle the raw data
+    window.addEventListener('deviceorientation', function(eventData) {
+      // gamma is the left-to-right tilt in degrees, where right is positive
+      var tiltLR = eventData.gamma / 180 * Math.PI;
+
+      // beta is the front-to-back tilt in degrees, where front is positive
+      var tiltFB = eventData.beta  / 180 * Math.PI;
+
+      // alpha is the compass direction the device is facing in degrees
+      var dir = eventData.alpha / 180 * Math.PI;
+
+      // call our orientation event handler
+      callback(tiltLR, tiltFB, dir);
+    }, false);
+  } else {
+    document.getElementById("doEvent").innerHTML = "Not supported."
+  }
+}
--- src/app.js
+++ src/app.js
@@ -1,24 +1,28 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera(
+  75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+object = new THREE.AxisHelper( 50 );
+object.position.set( 0, 0, 0 );
+scene.add( object );
 
 camera.position.z = 5;
+var updateCamera = function(tiltLR, tiltFB, dir) {
+  camera.rotation.x = tiltFB;
+  camera.rotation.y = tiltLR
+  // camera.rotation.z = dir;
+}
 
 var render = function () {
 	requestAnimationFrame( render );
 
-	cube.rotation.x += 0.1;
-	cube.rotation.y += 0.1;
-
 	renderer.render(scene, camera);
 };
+
+initDeviceOrientationWatcher(updateCamera);
 
 render();
